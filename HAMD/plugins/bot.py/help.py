afdae5a93386c89e3685b78e7077c1e93ef66524
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-10 16:33:54 +0300

    Update help.py

--- HAMD/plugins/bot.py/help.py
+++ HAMD/plugins/bot.py/help.py
@@ -1,27 +1,18 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
-
 from typing import Union
 
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
+import config
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import help_pannel
-from YukkiMusic.utils.database import get_lang, is_commanddelete_on
-from YukkiMusic.utils.decorators.language import (LanguageStart,
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils import help_pannel
+from AnonX.utils.database import get_lang, is_commanddelete_on
+from AnonX.utils.decorators.language import (LanguageStart,
                                                   languageCB)
-from YukkiMusic.utils.inline.help import (help_back_markup,
+from AnonX.utils.inline.help import (help_back_markup,
                                           private_help_panel)
 
 ### Command
