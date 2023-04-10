c4c7deb22655da4510043000f5fa47878b932469
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-10 04:59:56 +0300

    Update mute.py

--- HAMD/plugins/admins.py/mute.py
+++ HAMD/plugins/admins.py/mute.py
@@ -5,9 +5,8 @@ from config import BANNED_USERS
 from strings import get_command
 from AnonX import app
 from AnonX.core.call import Anon
-from AnonX.utils.database import is_music_playing, music_off
+from AnonX.utils.database import is_muted, mute_off
 from AnonX.utils.decorators import AdminRightsCheck
-from AnonX.utils.inline.play import close_keyboard
 
 # Commands
 MUTE_COMMAND = get_command("MUTE_COMMAND")
