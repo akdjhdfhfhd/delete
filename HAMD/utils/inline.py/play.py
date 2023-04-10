49f1e8f64011829f85eb6ebdb5d26d0816ad38aa
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-10 20:10:49 +0300

    Update play.py

--- HAMD/utils/inline.py/play.py
+++ HAMD/utils/inline.py/play.py
@@ -1,14 +1,12 @@
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+import math
 
-import random
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from pyrogram.types import InlineKeyboardButton
+import config
+from AnonX.utils.formatters import time_to_seconds
+
+
+## After Edits with Timer Bar
 
 selections = [
     "▁▄▂▇▄▅▄▅▃",
