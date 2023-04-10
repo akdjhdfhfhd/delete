58ca4e6d677208c1594cd7b2a686bf4da852b45f
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-10 17:46:53 +0300

    Update Private.py

--- HAMD/plugins/play.py/Private.py
+++ HAMD/plugins/play.py/Private.py
@@ -1,14 +1,20 @@
-#heloo 
+from pyrogram import filters
+from pyrogram.types import Message
 
-import asyncio
-from pyrogram import Client, filters
+import config
 from strings import get_command
-from strings.filters import command
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils.database import (add_private_chat,
+                                       get_private_served_chats,
+                                       is_served_private_chat,
+                                       remove_private_chat)
+from AnonX.utils.decorators.language import language
 
-from pyrogram.types import Message , ReplyKeyboardMarkup , KeyboardButton
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+AUTHORIZE_COMMAND = get_command("AUTHORIZE_COMMAND")
+UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
+AUTHORIZED_COMMAND = get_command("AUTHORIZED_COMMAND")
 
-#############################
 
 @app.on_message(filters.text & (filters.channel | filters.private))            
 async def hhhki(client: Client, message: Message):
