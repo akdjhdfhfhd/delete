74e53442e9b2e8243f705d08f7d7da32cd03f5c6
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-10 03:47:15 +0300

    Update mongo.py

--- HAMD/core/mongo.py
+++ HAMD/core/mongo.py
@@ -1,15 +1,5 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
-from pyrogram import Client
 
 import config
 
@@ -39,5 +29,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Yukki
-    pymongodb = _mongo_sync_.Yukki
+    mongodb = _mongo_async_.Anon 
+    pymongodb = _mongo_sync_.Anon 
